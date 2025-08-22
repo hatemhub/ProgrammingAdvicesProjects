850bea272b2aafe276720ed52ec34b36fdc9dbb9
Hatem <hatemshokr@outlook.com>
2025-08-22 17:25:06 +0300

    unsolved yet

---
+++ Introduction to programming using C++/26-Math_Functions.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //later
+    //16, 18, 19, 20, 21, 22, 23, 31, 32, 42, 43
+    /*
+    #16 : use pow inside sqrt.
+    #18,#19,#20: after finding the result use ceil and print the final results.
+    #21,#22: after finding the result use floor and print the final results.
+    #23 , #31 #32, #42, #43: after finding the result use round and print the final
+    results.
+    */
+}
